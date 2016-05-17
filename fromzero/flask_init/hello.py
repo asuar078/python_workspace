20ea04d01749504869537f698c5cf59e0c23c31d
Arian Ubuntu <asuar078@fiu.edu>
2016-05-16 20:14:51 -0400

    working with templates

--- fromzero/flask_init/hello.py
+++ fromzero/flask_init/hello.py
@@ -1,15 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask, url_for, request, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name_template=name)
 
 if __name__ == '__main__':
 
     host = os.getenv('IP', '0.0.0.0')
     port = int(os.getenv('PORT', 5000))
+    app.debug = True
     app.run(host=host, port=port)
